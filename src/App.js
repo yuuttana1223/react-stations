4c5f48ada9258054b92e4cfe99d8314ab26a480d
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-02-27 16:12:29 +0900

    ✨ station06

--- src/App.js
+++ src/App.js
@@ -4,13 +4,44 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
+
+const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random'
+
+const fetchDog = async () => {
+  const res = await fetch(DOG_API_URL)
+  if (!res.ok) {
+    throw Error(`Failed to fetch dog: ${res.status} ${res.statusText}`)
+  }
+  return res.json()
+}
+
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
+  const handleClick = React.useCallback(async () => {
+    try {
+      const dog = await fetchDog()
+      setDogUrl(dog.message)
+    } catch (error) {
+      console.error(error)
+      alert('犬の画像を取得できませんでした。')
+    }
+  }, [])
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header>
+        <h1>Dogアプリ</h1>
+      </header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src={dogUrl} alt="犬" />
+      <div>
+        <button onClick={handleClick}>表示</button>
+      </div>
+    </>
   )
 }
